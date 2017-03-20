650892f06d9c4a64649a2746461f94996e4d21d6
Oleh Pinta <oleg.sabfir@gmail.com>
2017-03-21 00:45:10 +0200

    add postoffice CRUD and API

---
+++ src/main/java/com/opinta/dao/PostOfficeDaoImpl.java
@@ -0,0 +1,48 @@
+package com.opinta.dao;
+
+import com.opinta.model.PostOffice;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PostOfficeDaoImpl implements PostOfficeDao {
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<PostOffice> getAll() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createCriteria(PostOffice.class)
+                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
+                .list();
+    }
+
+    @Override
+    public PostOffice getById(Long id) {
+        Session session = sessionFactory.getCurrentSession();
+        return (PostOffice) session.get(PostOffice.class, id);
+    }
+
+    @Override
+    public PostOffice save(PostOffice postOffice) {
+        Session session = sessionFactory.getCurrentSession();
+        return (PostOffice) session.merge(postOffice);
+    }
+
+    @Override
+    public void update(PostOffice postOffice) {
+        Session session = sessionFactory.getCurrentSession();
+        session.update(postOffice);
+    }
+
+    @Override
+    public void delete(PostOffice postOffice) {
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(postOffice);
+    }
+}
