2fb0733da6c5382b061ebe2c64858f20d212948d
Diarsid <diarsid@gmail.com>
2017-03-24 14:18:47 +0200

    init fake database restored.

--- src/main/java/com/opinta/model/BarcodeInnerNumber.java
+++ src/main/java/com/opinta/model/BarcodeInnerNumber.java
@@ -13,14 +13,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class BarcodeInnerNumber {
-    
     @Id
     @GeneratedValue
-    private long id;
-    
+    private long id;    
     @Size(min = 7, max = 7)
     private String number;
-    
     @Enumerated(EnumType.STRING)
     private BarcodeStatus status;
+    
+    public BarcodeInnerNumber(String number, BarcodeStatus status) {
+        this.number = number;
+        this.status = status;
+    }
 }
