d42da3809c332aa65cef00ecac81bdb0e35db96c
Diarsid <diarsid@gmail.com>
2017-03-28 13:36:23 +0300

    fixes.

--- src/main/java/com/opinta/temp/StartHsqlDbManager.java
+++ src/main/java/com/opinta/temp/StartHsqlDbManager.java
@@ -1,9 +1,13 @@
 package com.opinta.temp;
 
-//@Service
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class StartHsqlDbManager {
-//    @PostConstruct
-//    public void startDBManager() {
-//        DatabaseManagerSwing.main(new String[]{"--url", "jdbc:hsqldb:mem:testdb", "--user", "sa", "--password", "sa"});
-//    }
+    @PostConstruct
+    public void startDBManager() {
+        //DatabaseManagerSwing.main(new String[]{"--url", "jdbc:hsqldb:mem:testdb", "--user", "sa", "--password", "sa"});
+    }
 }
