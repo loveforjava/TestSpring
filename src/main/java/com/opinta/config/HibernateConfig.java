67775e09f74329922b1f78179243bb1e0190a340
Sameperson <ottosigma@gmail.com>
2017-03-31 13:49:19 +0300

    styles

--- src/main/java/com/opinta/config/HibernateConfig.java
+++ src/main/java/com/opinta/config/HibernateConfig.java
@@ -2,7 +2,11 @@ package com.opinta.config;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
