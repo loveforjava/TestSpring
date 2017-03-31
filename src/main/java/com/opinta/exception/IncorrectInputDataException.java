73420559a102dbda226c0a6a8031fcfa1fd0d1b4
Пінта Олег Олександрович <pinta-oo@ukrposhta.ua>
2017-03-31 13:40:10 +0300

    add some custom exception

---
+++ src/main/java/com/opinta/exception/IncorrectInputDataException.java
@@ -0,0 +1,20 @@
+package com.opinta.exception;
+
+public class IncorrectInputDataException extends Exception {
+
+    public IncorrectInputDataException() {
+        super();
+    }
+
+    public IncorrectInputDataException(String message) {
+        super(message);
+    }
+
+    public IncorrectInputDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IncorrectInputDataException(Throwable cause) {
+        super(cause);
+    }
+}
