35123f7b37e7ecbb08f211194acd6f5bdf0ac2b1
Sameperson <ottosigma@gmail.com>
2017-04-19 18:36:45 +0300

    fixes

--- src/main/resources/db/migration/common/V5.0__schema_user_counterparty.sql
+++ src/main/resources/db/migration/common/V5.0__schema_user_counterparty.sql
@@ -1,5 +1,5 @@
     alter table user_detail add counterparty_uuid raw(255) null;
---     alter table counterparty drop column user_id;
+    alter table counterparty drop column user_id;
 
     alter table user_detail
         add constraint FK_82nek8t2c2t4ikuwlx5ee6gga
