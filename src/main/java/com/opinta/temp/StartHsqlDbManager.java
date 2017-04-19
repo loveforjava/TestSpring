545c428b675bf319a24dd848cc5f024da9434eeb
Sameperson <ottosigma@gmail.com>
2017-04-19 19:05:03 +0300

    fixes

--- src/main/java/com/opinta/temp/StartHsqlDbManager.java
+++ src/main/java/com/opinta/temp/StartHsqlDbManager.java
@@ -1,9 +1,9 @@
 //package com.opinta.temp;
 //
-//        import org.hsqldb.util.DatabaseManagerSwing;
-//        import org.springframework.context.annotation.Profile;
-//        import org.springframework.stereotype.Service;
-//        import javax.annotation.PostConstruct;
+//import org.hsqldb.util.DatabaseManagerSwing;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.stereotype.Service;
+//import javax.annotation.PostConstruct;
 //
 //@Service
 //@Profile("memory")
