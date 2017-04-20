93789d9883491b937dc80bdbf5c528f73b9c02d0
Diarsid <diarsid@gmail.com>
2017-04-20 12:06:40 +0300

    Minor improvement - cannot reassign postId.

--- src/main/java/com/opinta/service/ClientServiceImpl.java
+++ src/main/java/com/opinta/service/ClientServiceImpl.java
@@ -159,6 +159,9 @@ public class ClientServiceImpl implements ClientService {
     public PostIdDto assignPostIdFor(UUID uuid, ClientType clientType, User user)
             throws IncorrectInputDataException, AuthException {
         Client client = getEntityByUuid(uuid, user);
+        if (client.getPostId() != null) {
+            return new PostIdDto(client.getPostId());
+        }
         PostIdDto postIdDto = new PostIdDto(generatePostIdUsing(clientType));
         client.setPostId(postIdDto.getPostId());
         clientDao.update(client);
