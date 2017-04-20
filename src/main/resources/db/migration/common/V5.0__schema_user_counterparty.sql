9aabcf2cd4fcece46ddffd3e577dccc8442f7c0d
Sameperson <ottosigma@gmail.com>
2017-04-20 10:17:45 +0300

    script for data moving

--- src/main/resources/db/migration/common/V5.0__schema_user_counterparty.sql
+++ src/main/resources/db/migration/common/V5.0__schema_user_counterparty.sql
@@ -1,4 +1,8 @@
     alter table user_detail add counterparty_uuid raw(255) null;
+
+    update user_detail
+    set counterparty_uuid = (select uuid from counterparty where user_id = user_detail.id);
+
     alter table counterparty drop column user_id;
 
     alter table user_detail
